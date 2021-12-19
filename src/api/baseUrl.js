48b7a31a658803c1e890b905282dba38494b8399
Hash Technologies <74411783+hashtechnologies-net@users.noreply.github.com>
2021-12-19 16:08:50 +0545

    Update baseUrl.js

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
-let baseURL = 'https://haminepal-api.herokuapp.com/api/v1/';
+let baseURL = 'https://api.haminepal.org/api/v1/';
 if (process.env.NODE_ENV === 'production')
-  baseURL = 'https://haminepal-api.herokuapp.com/api/v1/';
+  baseURL = 'https://api.haminepal.org/api/v1/';
 
 export default baseURL;
